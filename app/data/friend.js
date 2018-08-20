8dc8983a389237d367eba06e60e9cdfd6d681a9f
Ann Schulte <aschulte1919@gmail.com>
2018-08-20 16:28:33 -0600

    retry

---
+++ app/data/friend.js
@@ -0,0 +1,20 @@
+var friendList = [
+    {
+      name: 'Ann Schulte',
+      profilePic: '',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
